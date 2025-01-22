3d20d58d78982e1c5ad35f7fa0673485f1da8764
Modestas Vaitkevičius <info@modestasv.lt>
2025-01-22 16:27:33 +0200

    Fixed issues with namespaces

--- app/Http/Controllers/Vendor/WishlistController.php
+++ app/Http/Controllers/Vendor/WishlistController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Vendor;
 
+use App\Http\Controllers\Controller;
 use App\Models\Wishlist;
 
 class WishlistController extends Controller
